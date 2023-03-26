cea061aa6a7efb8d5512cc2d739396e337cdc481
James <divxnara1004@aver.com>
2023-03-26 14:12:53 +0900

    [스프링레거시 스터디] list페이지 개발
    Change-Id: I4fb895f4dd3e0e5e375a5f6e5368d1809ac4c47d

---
+++ BookMarket/src/main/java/com/springmvc/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.springmvc.controller;
+
+import com.springmvc.service.BookService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/books")
+public class BookController {
+
+	private final BookService bookService;
+
+
+	@GetMapping( "/list")
+	public ModelAndView getBookList() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject( "bookList", bookService.getAllBookList() );
+		mv.setViewName( "/books/list" );
+		return mv;
+	}
+}
